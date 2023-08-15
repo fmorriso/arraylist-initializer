82a03011dbf20fecf9e2f9a4deed7c7ab05b56b4
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-15 12:26:08 -0600

    reformat

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         // example of initializing an ArrayList in Java
-	    ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(56, 32, 48));
-	    System.out.println(nums);
+        ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(56, 32, 48));
+        System.out.println(nums);
     }
 }
