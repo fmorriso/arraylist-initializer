f93d3a03e9f4675538552216501f8dcf2edd7e2b
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-15 12:21:07 -0600

    push to GitHub

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // example of initializing an ArrayList in Java
+	    ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(56, 32, 48));
+	    System.out.println(nums);
+    }
+}
