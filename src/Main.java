198f2be5430206ba7370c24a40348da95e0d1c69
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-17 10:42:24 -0600

    add java version information

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,23 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        displayJavaVersionInformation();
         // example of initializing an ArrayList in Java
         ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(56, 32, 48));
-        System.out.println(nums);
+        System.out.format("ArrayList<Integer> nums: %s%n", nums);
+    }
+
+    private static void displayJavaVersionInformation() {
+        String version = System.getProperty("java.version");
+        System.out.println("java.version=" + version);
+
+        version = System.getProperty("java.specification.version");
+        System.out.println("java.specification.version=" + version);
+
+        version = System.getProperty("java.runtime.version");
+        System.out.println("java.runtime.version=" + version);
+        Runtime.Version runTimeVersion = Runtime.version();
+        System.out.println("RunTime.Version=" + runTimeVersion);
+        System.out.println("RunTime.Version.feature()=" + runTimeVersion.feature()); // major version
     }
 }
